fc11004b4ed04e66d043d4f94785d628354e0d36
Clark Chen <clark@kleric.org>
2017-11-26 16:56:26 -0800

    fixed by switching to single ?

--- Kanan/NoLogoutPenaltyMsg.cpp
+++ Kanan/NoLogoutPenaltyMsg.cpp
@@ -3,6 +3,6 @@
 namespace kanan {
 	NoLogoutPenaltyMsg::NoLogoutPenaltyMsg() : SimpleToggleMod("NoLogoutPenaltyMsg", "No Logout Penalty Message", "Automatically skips the \"If you exit now, you will lose 12911 EXP and 3131 Gold.\" warning message.")
 	{
-		addPatch("0F 84 ?? ?? ?? ?? 88 5D F2 88 5D F3", 0, { 0x90, 0xE9 });
+		addPatch("0F 84 ? ? ? ? 88 5D F2 88 5D F3", 0, { 0x90, 0xE9 });
 	}
 }
--- Kanan/NoSkillRankupWindow.cpp
+++ Kanan/NoSkillRankupWindow.cpp
@@ -4,6 +4,6 @@ namespace kanan {
 	NoSkillRankupWindow::NoSkillRankupWindow() : SimpleToggleMod("NoSkillRankupWindow", "No Skill Rankup Window", "Skips the popup after you advance a skill.")
 	{
 		addPatch("8b f0 33 db 3b f3 75 16 32 c0", 6, { 0x90, 0x90 });
-		addPatch("e8 ?? ?? ?? ?? 85 c0 0f 84 ?? ?? ?? ?? 8b 10 8b 35", 7, { 0x90, 0xE9 });
+		addPatch("e8 ? ? ? ? 85 c0 0f 84 ? ? ? ? 8b 10 8b 35", 7, { 0x90, 0xE9 });
 	}
 }
