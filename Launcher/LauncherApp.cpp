30ed35b274805c4882fc0dd7581d2701cfa5e522
cursey <cursey@live.com>
2019-03-07 21:04:26 -0800

    Fix launcher login endpoint

--- Launcher/LauncherApp.cpp
+++ Launcher/LauncherApp.cpp
@@ -380,7 +380,7 @@ void LauncherApp::mainUI() {
                         { "scope", "us.launcher.all" },
                         { "device_id", getDeviceID() }
                     }.dump() };
-                    auto response = httpPost("https://accounts.nexon.net/account/login/launcher", header, body);
+                    auto response = httpPost("https://www.nexon.com/account-webapi/login/launcher", header, body);
                     auto jsonResponse = json::parse(response);
                     auto token = jsonResponse.at("access_token").get<string>();
                     auto b64Token = stringEncode(CRYPT_STRING_BASE64, (const uint8_t*)token.data(), token.length());
