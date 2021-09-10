4279ae792b7f31e1f87eab949e4a654a126a0384
cursey <cursey@live.com>
2021-09-09 20:17:28 -0700

    Fix CWorld struct

--- Kanan/Mabinogi/CWorld.hpp
+++ Kanan/Mabinogi/CWorld.hpp
@@ -2,7 +2,7 @@
 #pragma pack(push, 1)
 class CWorld {
 public:
-    char pad_0[0xb0];
-    uint64_t localPlayerID; // 0xb0
-}; // Size: 0xb8
+    char pad_0[0xb8];
+    uint64_t localPlayerID; // 0xb8
+}; // Size: 0xc0
 #pragma pack(pop)
