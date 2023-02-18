79b29c993a0a7736ed826e5c6fddd38478e10753
poidoe <tarryntw@gmail.com>
2023-02-18 22:36:16 +1300

    Start of CookingMod updates
    Have found new scan
    currently waiting on safty hook to be updated in kanan.
    also need ot learn how to use inline hook

--- Kanan/CookingMod.hpp
+++ Kanan/CookingMod.hpp
@@ -1,7 +1,13 @@
 #pragma once
+#include "Kanan.hpp"
+#include <Scan.hpp>
+#include <Utility.hpp>
+#include <imgui.h>
 #include <vector>
 #include <windows.h>
 #include <TlHelp32.h>
+#include <Utility.hpp>
+#include <iostream>
 #include "Mod.hpp"
 namespace kanan {
 	class CookingMod :
@@ -13,17 +19,14 @@ namespace kanan {
 
 		void onUI() override;
 
-		void cookingMath(int cookingTemp[3]);
-
 
 		void onConfigLoad(const Config& cfg) override;
 		void onConfigSave(Config& cfg) override;
-
-		//void onKeyUp(DWORD key);
-
-		//void onKeyDown(DWORD key);
-
 	private:
 		bool m_is_enabled{};
+
+		int cookingOne = 0;
+		int cookingTwo = 0;
+		int cookingThree = 0;
 	};
 }
