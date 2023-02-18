48d7b3b9f475de961b90028c5361178685c5efab
poidoe <tarryntw@gmail.com>
2023-02-18 21:09:39 +1300

    Range Attack Swap Fix

--- Kanan/RangedAttackSwap.cpp
+++ Kanan/RangedAttackSwap.cpp
@@ -13,7 +13,7 @@ namespace kanan {
         m_choice{ 0 },
         m_patch{}
     {
-        auto address = scan("client.exe", "B8 09 52 00 00 5F");
+        auto address = scan("client.exe", "B8 09 52 00 00 48");
 
         if (address) {
             log("Got RangedAttackSwap %p", *address);
