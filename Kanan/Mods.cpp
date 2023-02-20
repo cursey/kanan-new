d4a4f2bbb326f9da8f1c6859a3d62f5348b8e2e3
cursey <cursey@live.com>
2023-02-20 00:06:42 -0800

    FreezeTimeOfDay: Fix for 64-bit

--- Kanan/Mods.cpp
+++ Kanan/Mods.cpp
@@ -126,8 +126,8 @@ namespace kanan {
         // addMod(make_unique<CookingMod>());
         addMod(make_unique<EquipmentOverride>());
         addMod(make_unique<FieldOfView>());
-        /* addMod(make_unique<FreezeTimeOfDay>());
-        addMod(make_unique<StatusUI>());
+        addMod(make_unique<FreezeTimeOfDay>());
+        /* addMod(make_unique<StatusUI>());
         addMod(make_unique<AutoChangeChannels>());
         addMod(make_unique<ChangeChannelHotkey>());*/
         addMod(make_unique<Currtarget>());
