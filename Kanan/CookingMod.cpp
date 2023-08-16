b8723376ea2ef04d06f281582da8ddead1b733ef
Kohu <peteratwater@gmail.com>
2023-08-15 23:02:36 -0700

    Update Cooking mod, Reforge Guide Skip, Unlimited zoom, Remove sunglare (#304)
    * Update disable waitprocesslocks
    
    Update disable waitprocess locks to fix an issue with enthralling performance and shooting rush movement.
    
    * Update reforge guide skip
    
    Update pattern for reforge guide skip initialization variable.
    
    * Update cooking mod
    
    Update cooking mod to hook the new functions called by the client. The old function that was hooked doesn't appear to be called anymore.
    
    * Update unlimited zoom
    
    Fix pattern for unlimited zoom
    
    * Update sunglare
    
    Update pattern for sunglare removal patch

--- Kanan/CookingMod.cpp
+++ Kanan/CookingMod.cpp
@@ -16,25 +16,35 @@ namespace kanan {
 
     static CookingMod* g_cooking;
 
-    void CookingMod::HookForCooking(int* _this, int ingredientIdx, int amount) {
+    //variable names may not accurately reflect the true names of the class. The original hooked function (see kanan pull request # 278)
+    //had args for this, ingredientIdx and Amount. Ingredient Idx is now part of the CookingViewClass (0x2FC). This is a pointer to CCookingView2 
+    void CookingMod::HookForCooking(int* _this, int amount) {
         if (!g_cooking->m_is_enabled) {
-            g_cooking->m_hook->callOriginal(_this, ingredientIdx, amount);
+            g_cooking->m_hook->callOriginal(_this, amount);
             return;
         }
-        g_cooking->m_hook->callOriginal(_this, 0, cookingOne);
-        g_cooking->m_hook->callOriginal(_this, 1, cookingTwo);
-        g_cooking->m_hook->callOriginal(_this, 2, cookingThree);
+        auto ingredientIdx = _this[193]; //Interface button we are currently clicking
+        if (ingredientIdx > -1 && ingredientIdx < 3) { //-1 is a state of not clicking any button. 0 through 2 are each Add button starting left
+            //Manually set each food percent of the object from our kanan UI
+                _this[188] = cookingOne;//+2F0
+                _this[189] = cookingTwo;
+                _this[190] = cookingThree;
 
-        //work around if you add some ingredient before enabled cooking mod, it may mess up amounts.
-        _this[188] = cookingOne;
-        _this[189] = cookingTwo;
-        _this[190] = cookingThree;
+                //subtract 1 from the button we are pressing to allow the the original function to set the cooking bar full flag correctly
+                //If we don't do this the code thast handles activating the start button does not get executed
+                _this[188 + ingredientIdx] = _this[188 + ingredientIdx] - 1;
+                //we call the original function - effecdtively adding the 1 value we just subtracted
+                g_cooking->m_hook->callOriginal(_this, amount);
+                return;
+        } else {
+            g_cooking->m_hook->callOriginal(_this, amount);
+            return;
+        }
     }  
 
     //find the addresses we need for patching
     CookingMod::CookingMod():m_hook(nullptr) {
-        auto cookingAddress = scan("client.exe", "40 57 48 83 EC 30 48 C7 44 24 20 FE FF FF FF 48 89 5C 24 40 48 89 74 24 48 48 8B F1 44 8B 89 F8 02 00 00 44 03 89 F4 02 00 00 44 03 89 F0 02 00 00 41 81 F9 40 42 0F 00 ");
-       
+    auto cookingAddress = scan("client.exe", "48 89 ? ? ? 48 89 ? ? ? 56 57 41 ? 48 83 EC ? 0F 29 ? ? ? 44 8B ? 48 8B ? 45 33 ? 0F B6 ? ? ? ? ? E8 ? ? ? ? 44 8B ? ? ? ? ? 41 83 F9 ? 0F 84 ? ? ? ? 41 83 F9 ? 0F 8D ? ? ? ? 8B 85 ? ? ? ? 85 C0 0F 84 ? ? ? ? 44 3B ? 0F 86 ? ? ? ? 41 8B ? 2B C8 0F AF ? ? ? ? ? B8 ? ? ? ? F7 E1 C1 EA ? 44 8B ? ? ? ? ? 44 03 ? ? ? ? ? 44 03 ? ? ? ? ? 41 81 F8 ? ? ? ? 0F 83 ? ? ? ? 41 8D ? ? B9 ? ? ? ? 41 2B ? 3D ? ? ? ? 0F 46 ? 85 C9 0F 84 ? ? ? ? 42 01 ? ? ? ? ? ? 48 8B ? E8 ? ? ? ? 84 C0 0F 84 ? ? ? ? 48 8B ? ? ? ? ? E8 ? ? ? ? 48 8B ? E8 ? ? ? ? 48 8B ? 48 C7 44 24 70 ? ? ? ? 48 8D ? ? ? ? ? E8 ? ? ? ? 8B F8 8D 48 ? E8 ? ? ? ? 48 89 ? ? ? 48 8D ? ? 4C 8D ? ? ? ? ? 8D 57 ? E8 ? ? ? ? 48 8B ? ? ? 89 79 ? 41 B0 ? 48 8D ? ? ? 48 8B ? E8 ? ? ? ? 90 48 8B ? ? ? 48 85 ? 74 ? E8 ? ? ? ? 48 C7 44 24 70 ? ? ? ? 41 B8 ? ? ? ? 48 8D ? ? ? ? ? 48 8B ? ? ? ? ? E8 ? ? ? ? 48 8B ? ? ? ? ? 48 8B ? B2 ? FF 90 ? ? ? ? 44 89 ? ? ? ? ? 48 8B ? ? ? ? ? 48 8B ? ? ? ? ? E8 ? ? ? ? 8B BD ? ? ? ? 03 BD ? ? ? ? 03 BD ? ? ? ? 48 85 ? 0F 84 ? ? ? ? 8D 4F ? 81 F9 ? ? ? ? 0F 87 ? ? ? ? 48 8B ? ? ? ? ? 48 8B ? E8 ? ? ? ? 48 8B ? E8 ? ? ? ? 0F 28 ? 48 8B ? E8 ? ? ? ? 0F 28 ? F3 0F ? ? ? ? ? ? 48 8B");
         if (!cookingAddress) {
             log("unable to find cooking address");
         }
@@ -68,11 +78,17 @@ namespace kanan {
 
     //config stuff
     void CookingMod::onConfigLoad(const Config& cfg) {
-        m_is_enabled = cfg.get<bool>("Cooking.Enabled").value_or(false);        
+        m_is_enabled = cfg.get<bool>("Cooking.Enabled").value_or(false);
+        cookingTemp[0] = cfg.get<int>("Cooking.cookingOne").value_or(0);
+        cookingTemp[1] = cfg.get<int>("Cooking.cookingTwo").value_or(0);  
+        cookingTemp[2] = cfg.get<int>("Cooking.cookingThree").value_or(0);  
     }
 
     void CookingMod::onConfigSave(Config& cfg) {
         cfg.set<bool>("Cooking.Enabled", m_is_enabled);
+        cfg.set<int>("Cooking.cookingOne", cookingTemp[0]);
+        cfg.set<int>("Cooking.cookingTwo", cookingTemp[1]);
+        cfg.set<int>("Cooking.cookingThree", cookingTemp[2]);
     } 
     
 }
